1dc54912af396a7f066b37909bf95efa75ecf260
keiro35 <s2200714@usls.edu.ph>
2026-09-07 10:18:04 +0800

    added encryption in login system

--- login/dashboard.php
+++ login/dashboard.php
@@ -16,6 +16,15 @@ if(!isset($_SESSION['username'])){
 </head>
 <body>
     <h2>Hello <?php echo $_SESSION['username']; ?>!</h2>
+    <p>Welcome to the dashboard page.</p>
+
+    <?php
+    $testhash = "testing";
+    echo "this is password_hash: " . password_hash($testhash, PASSWORD_DEFAULT);
+    echo"<br>";
+    echo "this is md5: " . md5($testhash);
+
+    ?>
     <a href = "logout.php">Logout</a>
 </body>
 </html>
